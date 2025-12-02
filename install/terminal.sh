9d0038de1aff0f3deb4a30c82332d9683af33c61
Paul Bennett <p@mrpbennett.dev>
2025-12-02 16:10:11 +0000

    another typo

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run terminal installers
-for installer in ~/omakub/install/terminal/*.sh; do source $installer; done
+for installer in ~/umaku/install/terminal/*.sh; do source $installer; done
