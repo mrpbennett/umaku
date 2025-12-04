a4e8515192d0530ef5840584d2840a07ecad2a72
Paul Bennett <p@mrpbennett.dev>
2025-12-04 14:33:41 +0000

    ..

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -12,7 +12,3 @@ cp ~/.local/share/umaku/configs/.zshrc ~/.zshhrc
 [ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
 # Configure the inputrc using Umaku defaults
 cp ~/.local/share/umaku/configs/inputrc ~/.inputrc
-
-# ZSH auto suggestions & syntax highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
