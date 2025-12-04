b120f49984abef71a2223a3b6627f572b6c3aaf6
Paul Bennett <p@mrpbennett.dev>
2025-12-04 14:30:15 +0000

    ..

--- install/terminal/oh-my-zsh.sh
+++ install/terminal/oh-my-zsh.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# remove director if it exists
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    rm -r "$HOME/.oh-my-zsh"
+fi
+
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
