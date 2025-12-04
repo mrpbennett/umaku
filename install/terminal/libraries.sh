e0d05a2db986cf932227faa438b79a54dae64049
Paul Bennett <p@mrpbennett.dev>
2025-12-04 14:39:14 +0000

    pipx fix

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -4,3 +4,5 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang rustc pipx \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools
+
+pipx ensurepath
