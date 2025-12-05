c2200ed5f9360dcd03e7a28182594dc629fcdf73
Paul Bennett <p@mrpbennett.dev>
2025-12-05 11:31:42 +0000

    fixing zshrc bug

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -5,7 +5,14 @@ sudo apt install zsh -y
 # make zsh default
 sudo chsh -s $(which zsh) $USER
 
-# Configure the bash shell using umaku defaults
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# zsh syntax highlighting and auto suggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Configure the zsh shell using umaku defaults
 [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
 cp ~/.local/share/umaku/configs/zshrc ~/.zshrc
 
@@ -15,10 +22,3 @@ source ~/.local/share/umaku/defaults/zsh/shell
 [ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
 # Configure the inputrc using umaku defaults
 cp ~/.local/share/umaku/configs/inputrc ~/.inputrc
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# zsh syntax highlighting and auto suggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
