940743c44aaa183304dda323e38e28d602ad721c
Paul Bennett <p@mrpbennett.dev>
2025-12-05 11:02:17 +0000

    oops

--- themes/catppuccin/gnome.sh
+++ themes/catppuccin/gnome.sh
@@ -2,7 +2,6 @@
 
 UMAKU_THEME_COLOR="magenta"
 UMAKU_THEME_BACKGROUND="catppuccin/background.png"
-source $UMAKU_PATH/themes/set-gnome-theme.sh
 
 # Set the Gnome theme
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
